89641337d95a5ecf78b1d10ab1d463f1ffc47a1b
Andre Garcia <andrecadgarcia@gmail.com>
2022-05-09 22:05:54 -0300

    setup tests with enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders', () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper).toBeTruthy();
+  });
 });
